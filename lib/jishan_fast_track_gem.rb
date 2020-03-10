d10c623d293b4f4104359714cc1835407cef9124
Ji Shan <uug54332017@gmail.com>
2020-03-10 22:08:49 +1100

    updated eligibility check

--- lib/jishan_fast_track_gem.rb
+++ lib/jishan_fast_track_gem.rb
@@ -1,5 +1,157 @@
-require "jishan_fast_track_gem/version"
+# require "jishan_fast_track_gem/version"
+require 'uri'
+require 'csv'
 
-puts "hello World"
 
-#first file you have written should be here.
+
+class Solar_panel_rebate_egilibility_check
+  def initialize(name)
+    @name = name
+  end
+
+  def ower_of_the_property
+    puts "Are you the property owner and the current occupier?"
+    puts "1. yes"
+    puts "2. no"
+    answer = gets.chomp.to_i
+    if answer == 1
+      puts "check the next question."
+      received_solar_rebate_before
+    elsif answer == 2
+      puts "sorry, your are not eligible."
+    else
+      puts "no valid input."
+    end
+  end
+  
+  def received_solar_rebate_before
+    puts "Have you received solar panel rebate before?"
+    puts "1. yes"
+    puts "2. no"
+    answer = gets.chomp.to_i
+    if answer == 1
+      puts "sorry, your are not eligible."
+    elsif answer == 2
+      puts "check the next question."
+      existed_solar_panel
+    else
+      puts "no valid input."
+    end
+  end
+  def existed_solar_panel
+    puts "Do you have an existed solar panel?"
+    puts "1. yes"
+    puts "2. no"
+    answer = gets.chomp.to_i
+    if answer == 1
+      puts "sorry, your are not eligible."
+    elsif answer == 2
+      puts "check the next question."
+      combined_houshold_income
+    else
+      puts "no valid input."
+    end
+  end
+  def combined_houshold_income
+   puts "Is your household income below $180,000aud ?"
+    puts "1. yes"
+    puts "2. no"
+    answer = gets.chomp.to_i
+    if answer == 1
+      puts "check the next question."
+      property_value
+    elsif answer == 2
+      puts "sorry, your are not eligible."
+    else
+      puts "no valid input."
+    end
+  end
+  def property_value
+    puts "Is your property value more than $3 millions ?" 
+    puts "1. yes"
+    puts "2. no"
+    answer = gets.chomp.to_i
+    if answer == 1
+      puts "sorry, your are not eligible."
+    elsif answer == 2
+      puts "congrats! You are eligible for this rebate!"
+    else
+      puts "no valid input."
+    end
+  end
+end
+
+class Provided_files_check
+  def initialize(name)
+    @name = name
+  end
+  def income_proof
+    puts "provide the proof of income document URL (only .pdf or .doc): "
+    url = gets.chomp
+    file_type = File.extname(URI.parse(url).path)
+    if file_type == ".pdf" || file_type == ".doc"
+      puts "file upload succeeded." 
+      two_forms_of_identity
+    else 
+      puts "upload failed. please provide valid URL to .pdf or .doc ."
+    end
+  end
+
+  def two_forms_of_identity
+    file_arr = []
+    loop do
+      puts "provide identity documents' URL (only .pdf or .doc): "
+      url = gets.chomp
+      file_type = File.extname(URI.parse(url).path)
+      if file_type == ".pdf" || file_type == ".doc"
+        file_arr << file_type
+        puts "file upload succeeded." 
+      else 
+        puts "upload failed. please provide valid URL to .pdf or .doc ."
+      end
+      p file_arr
+    break if file_arr.size == 2
+    end
+    eligible_quote
+  end
+
+  def retailer_quote
+    puts "Provide the eligible quote URL (only .pdf or .doc): "
+    url = gets.chomp
+    file_type = File.extname(URI.parse(url).path)
+
+      if file_type == ".pdf" || file_type == ".doc"
+        puts "file upload succeeded." 
+        eligible_quote(url)
+      else 
+        puts "upload failed. please provide valid URL to .pdf or .doc ."
+      end
+  end    
+  def eligible_quote(url)
+    retailer = URI.parse(url).host
+    csv_text = File.read('cec_approved_retailers.csv')
+    csv = CSV.parse(csv_text, headers: true)
+    
+      result = csv.find do |web|
+      web_address = web.to_hash
+      retailer == web_address['URL']
+      end 
+
+      if result != nil
+        puts "Eligible retailer."
+      else
+        puts "Not Eligible retailer."  
+     end
+  end
+end
+
+# ruby = Solar_panel_rebate_egilibility_check.new("Ruby")
+# ruby.ower_of_the_property
+
+ruby_files = Provided_files_check.new("ruby_files")
+# ruby_files.income_proof
+ruby_files.retailer_quote
+
+
+
+
