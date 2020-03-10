af9cbbd9cb1b15e3c183267350675d4e55b0714d
Ji Shan <uug54332017@gmail.com>
2020-03-10 14:00:45 +1100

    first change

---
+++ lib/jishan_fast_track_gem.rb
@@ -0,0 +1,8 @@
+require "jishan_fast_track_gem/version"
+
+module JishanFastTrackGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+#first file you have written should be here.
---
+++ lib/jishan_fast_track_gem/version.rb
@@ -0,0 +1,3 @@
+module JishanFastTrackGem
+  VERSION = "0.1.0"
+end
