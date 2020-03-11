a17da7158b665f1b0f0142c58cae4b4df4662eaa
Ji Shan <uug54332017@gmail.com>
2020-03-12 08:13:12 +1100

    colorized eligibility check

--- lib/eligibility_check.rb
+++ lib/eligibility_check.rb
@@ -10,72 +10,59 @@ class Solar_panel_rebate_egilibility_check
   
   def ower_of_the_property
     puts "Are you the property owner and the current occupier?".colorize(:light_blue)
-    puts "1. yes".colorize(:light_blue)
-    puts "2. no".colorize(:light_blue)
+    puts "1. Yes 2. No".colorize(:light_blue)
     answer = gets.chomp.to_i
-    if answer == 1
-      puts "check the next question."
-      received_solar_rebate_before
-    elsif answer == 2
-      puts "sorry, your are not eligible."
-    else
-      puts "no valid input."
-    end
+    return unless answer_check(answer)
+    received_solar_rebate_before
   end
   
   def received_solar_rebate_before
-    puts "Have you received solar panel rebate before?"
-    puts "1. yes"
-    puts "2. no"
+    puts "Is this your first apply for solar panel rebate?".colorize(:light_blue)
+    puts "1. Yes 2. No".colorize(:light_blue)
     answer = gets.chomp.to_i
-    if answer == 1
-      puts "sorry, your are not eligible."
-    elsif answer == 2
-      puts "check the next question."
-      existed_solar_panel
-    else
-      puts "no valid input."
-    end
+    return unless answer_check(answer)
+    existed_solar_panel
   end
+
   def existed_solar_panel
-    puts "Do you have an existed solar panel?"
-    puts "1. yes"
-    puts "2. no"
+    puts "Is this your first solar panel installation?".colorize(:light_blue)
+    puts "1. Yes 2. No".colorize(:light_blue)
+
     answer = gets.chomp.to_i
-    if answer == 1
-      puts "sorry, your are not eligible."
-    elsif answer == 2
-      puts "check the next question."
-      combined_houshold_income
-    else
-      puts "no valid input."
-    end
+    return unless answer_check(answer)
+    combined_houshold_income
   end
+
   def combined_houshold_income
-   puts "Is your household income below $180,000aud ?"
-    puts "1. yes"
-    puts "2. no"
+    puts "Is your household income below $180,000aud?".colorize(:light_blue)
+    puts "1. Yes 2. No".colorize(:light_blue)
+    answer = gets.chomp.to_i
+    return unless answer_check(answer)
+    property_value
+  end
+  
+  def property_value
+    puts "Is your property value less than $3 millions?" .colorize(:light_blue)
+    puts "1. Yes 2. No".colorize(:light_blue)
     answer = gets.chomp.to_i
     if answer == 1
-      puts "check the next question."
-      property_value
+      puts "congrats! You are eligible for this rebate!".colorize(:light_red)
     elsif answer == 2
-      puts "sorry, your are not eligible."
+    puts "Sorry, your are not eligible."
     else
       puts "no valid input."
     end
   end
-  def property_value
-    puts "Is your property value more than $3 millions ?" 
-    puts "1. yes"
-    puts "2. no"
-    answer = gets.chomp.to_i
+  def answer_check(answer)
     if answer == 1
-      puts "sorry, your are not eligible."
+      puts "Please answer the next question.".colorize(:light_blue)
+      return true
     elsif answer == 2
-      puts "congrats! You are eligible for this rebate!"
+      puts "Sorry, your are not eligible."
+      return false
     else
-      puts "no valid input."
+      puts "It is no a valid input."
+      return false
     end
   end
 end
--- lib/jishan_fast_track_gem.rb
+++ lib/jishan_fast_track_gem.rb
@@ -8,10 +8,10 @@ require_relative 'manage_lists'
 # Help file includes *comprehensive* instructions which *accurately* describe: steps to install the application; dependencies required by the application; system/hardware requirements; features of the application.
 
 ruby = Solar_panel_rebate_egilibility_check.new("Ruby")
-# ruby.ower_of_the_property
+ruby.ower_of_the_property
 
 john_files = Provided_files_check.new("john_files")
-john_files.two_forms_of_identity
+# john_files.two_forms_of_identity
 john_rebate = Rebate_calculator.new
 # john_rebate.stc_postcode_rating(3000)
 # john_rebate.stc_calculator(5, 5)
